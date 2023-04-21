6c81b4c1263d4f35a495481a69a522026adc90e7
Albert <albertirura11@gmail.com>
2023-04-21 13:26:59 -0700

    free mallocs in file_finder.c

--- file_finder.c
+++ file_finder.c
@@ -13,12 +13,13 @@
 
 int file_finder(char *tokens[], char *environ[])
 {
-	char **path_token, *delim = ":", *cp1;
+	char **path_token, *delim = ":", *cp1, *path;
 	int i = 0;
 	DIR *fd;
 	struct dirent *entry;
 
-	path_token = the_tokeniser(getpath(environ), delim);
+	path = getpath(environ);
+	path_token = the_tokeniser(path, delim);
 	cp1 = stringcpy(tokens[0]);
 
 	while (path_token[i] != NULL)
@@ -26,23 +27,20 @@ int file_finder(char *tokens[], char *environ[])
 		fd = opendir(path_token[i]);
 		if (fd == NULL)
 		{
-			free_grid(path_token);
-			free(cp1);
+			free_ff(path_token, path, cp1);
 			return (-1);
 		}
 		while ((entry = readdir(fd)) != NULL)
 		{
 			if ((strcmp(entry->d_name, cp1)) == 0)
 			{
-				free_grid(path_token);
-				free(cp1);
+				free_ff(path_token, path, cp1);
 				return (0);
 			}
 		}
 		closedir(fd);
 		i++;
 	}
-	free_grid(path_token);
-	free(cp1);
+	free_ff(path_token, path, cp1);
 	return (-1);
 }
--- the_tokeniser.c
+++ the_tokeniser.c
@@ -29,5 +29,6 @@ char **the_tokeniser(char *cmd, char *delim)
 		i++;
 	}
 	tokens[i] = NULL;
+	free(cmd_cp);
 	return (tokens);
 }
