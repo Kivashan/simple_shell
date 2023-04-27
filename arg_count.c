adb2fce45b87a8c2a7857ba889ca2f2087ed3640
Kivashan <bvkivashan1982@gmail.com>
2023-04-27 14:03:07 -0700

    Resolved betty issue with arg_count.c

--- arg_count.c
+++ arg_count.c
@@ -2,13 +2,14 @@
 
 /**
  * arg_count - count the number of times that commands passed in the shell
+ * @flag: a flag to determine if count is incremented
  *
  * Return: The number of times commands are inputed in the shell
  */
 
 char arg_count(int flag)
 {
-	static int count = 0;
+	static int count = 1;
 	char i = '0';
 
 	i = i + count;
